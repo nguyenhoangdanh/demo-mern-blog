363b6b8a1abe506a7b4ae56aafef83af22320135
nguyenhoangdanh.io <hoangdanh54317@gmail.com>
2024-02-02 12:42:06 +0700

    add theme

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,18 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Button, Navbar, Avatar, TextInput, Dropdown } from 'flowbite-react'
 import path from 'path'
 import React from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
-import { Link, useLocation } from 'react-router-dom'
+import { FaMoon, FaSun} from 'react-icons/fa'
+import { Link, useLocation } from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
+import { toggleTheme } from "../redux/theme/themeSlice";
+
 
 export const Header = () => {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
   return (
     <Navbar className='border-b-2'>
         <Link className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white' to="/">
@@ -25,14 +31,42 @@ export const Header = () => {
           <AiOutlineSearch />
         </Button>
         <div className="flex gap-2 md:order-2">
-          <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-            <FaMoon />
+          <Button className='w-12 h-10 hidden sm:inline' 
+          color='gray' 
+          pill
+          onClick={() => dispatch(toggleTheme())}
+          >
+             {theme === 'light' ? <FaSun /> : <FaMoon />}
           </Button>
-          <Link to={'/sign-in'}>
-            <Button gradientDuoTone={"purpleToBlue"}>
+          {currentUser ? (
+          <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt='user' img={currentUser.profilePicture} rounded />
+          }
+          >
+             <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item 
+            // onClick={handleSignout}
+            >Sign out</Dropdown.Item>
+            </Dropdown>
+          ):(
+            <Link to={'/sign-in'}>
+            <Button gradientDuoTone={"purpleToBlue"} outline>
               Sign In
             </Button>
           </Link>
+          )}
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
