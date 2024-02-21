5c0224dad236992d9e79797f3595372295e02698
nguyenhoangdanh.io <hoangdanh54317@gmail.com>
2024-02-21 13:03:59 +0700

    update post

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../ultils/verifyUser.js';
-import { createPost, deletePost, getPosts } from '../controller/post.controller.js';
+import { createPost, deletePost, getPosts, updatePost } from '../controller/post.controller.js';
 
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/create', verifyToken, createPost);
 router.get('/getposts', getPosts);
 router.delete('/delete-post/:postId/:userId', verifyToken, deletePost)
+router.put('/update-post/:postId/:userId', verifyToken, updatePost)
 
 
 export default router;
